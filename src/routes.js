16fd54813ab23d02a840289cd000afcd90a2156b
heresson <heresson@gmail.com>
2019-08-01 23:26:40 -0300

    criando caminho pra upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,24 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
+
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+routes.post('/users', UserController.store);
+
+routes.post('/files', FileController.store);
 
 export default routes;
